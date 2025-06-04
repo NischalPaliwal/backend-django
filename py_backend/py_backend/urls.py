b1f5feb70bef3604dadb288d3efc09d2dcda9f68
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-06-04 13:39:54 +0530

    Fix formatting issues in layout.html and update settings and urls for consistency

--- py_backend/py_backend/urls.py
+++ py_backend/py_backend/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('app/', include('app.urls'))
+    path('app/', include('app.urls')),
 ]
