e6366178a62259f4ee98096cbb70593ac1da3087
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-06-12 10:45:45 +0530

    code added

--- py_backend/app/models.py
+++ py_backend/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Cars(models.Model):
+    car_type_choice = [('HTB', 'Hatchback'), 
+                       ('SD', 'Sedan'), 
+                       ('SUV', 'SUV'), 
+                       ('MUV', 'MUV'), 
+                       ('LX', 'Luxury')]
+    
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='cars/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=3, choices=car_type_choice)
+
+    def __str__(self):
+        return self.name
