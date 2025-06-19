e55a1e479f04318d20100afa582225b3982f7f2e
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2025-06-19 09:37:42 +0530

    code added

--- py_backend/app/admin.py
+++ py_backend/app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Cars
+from .models import CarReview
+from .models import Showroom
+from .models import SafetyCertificate
 
 admin.site.register(Cars)
+admin.site.register(CarReview)
+admin.site.register(Showroom)
+admin.site.register(SafetyCertificate)
